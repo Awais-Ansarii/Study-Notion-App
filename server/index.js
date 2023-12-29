2fcdc598dce3bb2723a9a599824884323d32ed19
Awais-Ansarii <awaisansari1502@gmail.com>
2023-12-29 17:42:59 +0530

    created a Profile model schema

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 //server connection
 app.listen(PORT, () => {
-    console.log(`Server is runing on port-${PORT}`)
+    console.log(`Server is running on port-${PORT}`)
 })
 
 // DB connection
---
+++ server/models/Profile.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const profileSchema = new mongoose.Schema({
+  gender: {
+    type: String,
+    required: true,
+  },
+
+  dateOfBirth: {
+    type: String,
+  },
+
+  about: {
+    type: String,
+    trim: true,
+  },
+  contactNumber: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  address: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+module.exports = mongoose.model("Profile", profileSchema);
