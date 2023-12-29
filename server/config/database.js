79e7d7135356b0b9e26e7c3d736fd1190ee8e2db
Awais-Ansarii <awaisansari1502@gmail.com>
2023-12-29 17:17:27 +0530

    initiated express app, connected with DB, initiated server

--- server/config/database.js
+++ server/config/database.js
@@ -1,1 +1,20 @@
 
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const DB_URL = process.env.DB_URL;
+
+function dbConnection() {
+    mongoose.connect(DB_URL, { family: 4 })
+    .then(()=>console.log("Database Connected"))
+    .catch((err) => {
+        console.log("Error while connecting to the Database");
+        console.error(err);
+        process.exit(1);
+    })
+};
+
+
+module.exports = dbConnection;
+
--- server/index.js
+++ server/index.js
@@ -1,1 +1,25 @@
-console.log("hi")
+const express = require("express");
+const dotenv = require("dotenv");
+const dbConnection = require('./config/database')
+
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT;
+
+//middleware - body parser
+app.use(express.json());
+
+//server connection
+app.listen(PORT, () => {
+    console.log(`Server is runing on port-${PORT}`)
+})
+
+// DB connection
+
+
+//default route
+app.get('/', (req, res) => {
+    res.send('<h1>Study Notion App</h1>')
+})
