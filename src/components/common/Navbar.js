993c12c90ca416d5684b31834c83f699d1242888
Awais-Ansarii <awaisansari1502@gmail.com>
2024-01-31 00:31:31 +0530

    updated navbar

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, matchPath, useLocation } from "react-router-dom";
 import Logo from "../../assets/Logo/Logo-Full-Light.png";
 import { NavbarLinks } from "../../data/navbar-links";
 import { useSelector } from "react-redux";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import { IoIosArrowDropdownCircle } from "react-icons/io";
 import ProfileDropdown from "../core/Auth/ProfileDropdown";
+import { apiConnector } from "../../services/apiconnector";
+import { categories } from "../../services/apis";
+
 const Navbar = () => {
   const { token } = useSelector((state) => state.auth);
   const { user } = useSelector((state) => state.profile);
   const { totalItems } = useSelector((state) => state.cart);
 
   const location = useLocation();
+
+  const [subLinks, setSubLinks] = useState([]);
+
+  const fetchSubLinks = async () => {
+    try {
+      const result = await apiConnector("GET", categories.CATEGORIES_API);
+      // console.log("printing sublinks result: ", result);
+      setSubLinks(result.data.data);
+    } catch (error) {
+      console.log("Could not fetch the category list via api call");
+      console.log(error);
+    }
+  };
+  console.log("printing sublinks : ", subLinks);
+
+  useEffect(() => {
+    fetchSubLinks();
+  }, []);
 
   const matchRoute = (route) => {
     return matchPath({ path: route }, location.pathname);
@@ -30,10 +52,33 @@ const Navbar = () => {
               return (
                 <li key={index}>
                   {link.title === "Catalog" ? (
+                    <div className="relative flex items-center gap-2 cursor-pointer group">
+                      <p>{link.title}</p>
+                      <IoIosArrowDropdownCircle className="" />
 
-                    <div>{link.title}</div>
+                      <div
+                        className="invisible  absolute left-[50%] translate-x-[-50%]
+                      top-[50%] translate-y-[36%] z-50 flex flex-col rounded-md text-richblack-900 opacity-0 transition-all duration-200 bg-richblack-5 p-4 group-hover:visible group-hover:opacity-100 lg:w-[300px]"
+                      >
+                        <div className="absolute left-[50%] top-0  translate-y-[-45%] translate-x-[80%] h-6 w-6  rotate-45 rounded bg-richblack-5"></div>
 
-                    
+                        {subLinks.length ? (
+                          subLinks.map((category, index) => {
+                            return (
+                              <Link
+                                className=""
+                                to={`/catelog/${category.name.toLowerCase()}`}
+                                key={index}
+                              >
+                                <p>{category.name}</p>
+                              </Link>
+                            );
+                          })
+                        ) : (
+                          <div></div>
+                        )}
+                      </div>
+                    </div>
                   ) : (
                     <Link to={link?.path}>
                       <p
