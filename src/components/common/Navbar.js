c9b97a6744034adf28b1271cf0d176db8a692e95
Awais-Ansarii <awaisansari1502@gmail.com>
2024-01-30 22:29:02 +0530

    created signup login button link

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -2,15 +2,23 @@ import React from "react";
 import { Link, matchPath, useLocation } from "react-router-dom";
 import Logo from "../../assets/Logo/Logo-Full-Light.png";
 import { NavbarLinks } from "../../data/navbar-links";
-
+import { useSelector } from "react-redux";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import ProfileDropdown from "../core/Auth/ProfileDropdown";
 const Navbar = () => {
+  const { token } = useSelector((state) => state.auth);
+  const { user } = useSelector((state) => state.profile);
+  const { totalItems } = useSelector((state) => state.cart);
+
   const location = useLocation();
+
   const matchRoute = (route) => {
     return matchPath({ path: route }, location.pathname);
   };
+
   return (
-    <div className=" h-14  border-b-[2px] border-richblack-700 flex justify-center items-center">
-      <div className="w-11/12 max-w-maxContent mx-auto flex justify-between ">
+    <div className=" h-16  border-b-[2px] border-richblack-700 flex justify-center items-center">
+      <div className="w-11/12 max-w-maxContent mx-auto flex justify-between items-center">
         <Link to="/">
           <img src={Logo} alt="logo" width={160} height={42} loading="lazy" />
         </Link>
@@ -22,7 +30,10 @@ const Navbar = () => {
               return (
                 <li key={index}>
                   {link.title === "Catalog" ? (
+
                     <div>{link.title}</div>
+
+                    
                   ) : (
                     <Link to={link?.path}>
                       <p
@@ -40,13 +51,47 @@ const Navbar = () => {
               );
             })}
           </ul>
-              </nav>
-              
+        </nav>
 
-              {/* login signup dashboard */}
-              <div className="flex gap-x-4 items-center">
-                  
-              </div>
+        {/* login signup dashboard */}
+        <nav>
+          <div className="flex gap-x-4 ">
+            {
+              //show cart
+              user && user?.accountType !== "Instructor" && (
+                <Link to="/dashboard/cart" className="relative">
+                  <AiOutlineShoppingCart className="text-yellow-25" />
+                  {totalItems > 0 && <span>{totalItems}</span>}
+                </Link>
+              )
+            }
+
+            {
+              //show login button
+              token === null && (
+                <Link to="/login">
+                  <button className="border border-richblack-700 bg-richblack-800 text-richblack-25 rounded px-[12px] py-[8px]">
+                    Login
+                  </button>
+                </Link>
+              )
+            }
+            {
+              //show SignUp button
+              token === null && (
+                <Link to="/signup">
+                  <button className="border border-richblack-700 bg-richblack-800 text-richblack-100 rounded px-[12px] py-[8px]">
+                    Sign Up
+                  </button>
+                </Link>
+              )
+            }
+            {
+              //show SignUp button
+              token !== null && <ProfileDropdown />
+            }
+          </div>
+        </nav>
       </div>
     </div>
   );
